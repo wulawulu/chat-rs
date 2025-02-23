5f7f4971721ed4d802a33949b986e611de5460a8
wulawulu <wutuobangbang@gmail.com>
2025-02-23 16:36:00 +0800

    feature: notify server

---
+++ notify-server/src/main.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use notify_server::get_router;
+use tokio::net::TcpListener;
+use tracing::info;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Layer as _;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let addr = "0.0.0.0:6687";
+
+    let app = get_router();
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening on: {}", addr);
+
+    axum::serve(listener, app.into_make_service()).await?;
+
+    Ok(())
+}
