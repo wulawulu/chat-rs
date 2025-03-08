91296efc3c7def92e2abaccf1017438129eb93fb
wulawulu <w2473698581@163.com>
2025-03-08 20:19:51 +0800

    feature: support message create

--- chat-server/src/model/mod.rs
+++ chat-server/src/model/mod.rs
@@ -4,6 +4,7 @@ use sqlx::FromRow;
 
 mod chat;
 mod file;
+mod messages;
 mod user;
 mod workspace;
 
@@ -61,4 +62,14 @@ pub struct ChatFile {
     pub ws_id: u64,
     pub ext: String,
     pub hash: String,
+}
+
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq)]
+pub struct Message {
+    pub id: i64,
+    pub chat_id: i64,
+    pub sender_id: i64,
+    pub content: String,
+    pub files: Vec<String>,
+    pub created_at: DateTime<Utc>,
 }
